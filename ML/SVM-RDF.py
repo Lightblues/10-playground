4edfa25859dbf517cdd7966605a8c0d08ca5a437
easonshi <oldcitystal@gmail.com>
2021-07-26 00:27:53 +0800

    add Spyder/Waijiaobu

---
+++ ML/SVM-RDF.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 24 11:02:23 2021
+
+@author: easonshi
+
+from https://louyu.cc/articles/machine-learning/2019/10/?p=2087/ 
+"""
+
+from sklearn.svm import SVC  # 这里直接导入SVC，而不是LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+# 加载moons数据集，形状是两个半月形，并加上噪音
+X, y = datasets.make_moons(noise=0.15, random_state=666)
+
+
+def PolynomialSVC(degree, C=1.0):
+    return Pipeline([
+        ("poly", PolynomialFeatures(degree=degree)),  # 使用多项式特征
+        ("std_scaler", StandardScaler()),  # 进行标准化
+        ("linearSVC", LinearSVC(C=C))       # C 为正则项
+    ])
+
+
+poly_svc = PolynomialSVC(degree=3)      # 三阶
+poly_svc.fit(X, y)
+
+
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int(
+            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int(
+            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+
+    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
+
+
+plot_decision_boundary(poly_svc, axis=[-1.5, 2.5, -1.0, 1.5])
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.show()
+
+
+# %%
+
+
+def PolynomialKernelSVC(degree, C=1.0):
+    return Pipeline([
+        ("std_scaler", StandardScaler()),
+        ("kernelSVC", SVC(kernel="poly", degree=degree, C=C))  # 核函数传入poly，也就是多项式核的意思
+    ])
+
+
+poly_kernel_svc = PolynomialKernelSVC(degree=3, C=1)
+poly_kernel_svc.fit(X, y)
+
+plot_decision_boundary(poly_kernel_svc, axis=[-1.5, 2.5, -1.0, 1.5])
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.show()
+
+
+# %%
+# 高斯核函数
+
+def RBFKernelSVC(gamma):
+    return Pipeline([
+        ("std_scaler", StandardScaler()),  # 进行标准化处理
+        ("svc", SVC(kernel="rbf", gamma=gamma))  # 核函数采用rbf核
+    ])
+
+
+svc = RBFKernelSVC(gamma=1)     # gamma越大，高斯分布越集中（倾向 overfit）
+svc.fit(X, y)
+
+
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int(
+            (axis[1] - axis[0]) * 100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int(
+            (axis[3] - axis[2]) * 100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+
+    plt.contourf(x0, x1, zz, linewidth=5, cmap=custom_cmap)
+
+
+plot_decision_boundary(svc, axis=[-1.5, 2.5, -1.0, 1.5])
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.show()
+
+
+# %%
---
+++ Spyder/Waijiaobu.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from tqdm import tqdm
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+prefix = "http://new.fmprc.gov.cn/web/fyrbt_673021/jzhsl_673025/"
+listpage = "default_{}.shtml"
+
+def get_onelist(pageindex):
+    response = requests.get(prefix + pageindex)
+    response.encoding = "utf8"      # 手动设置 encoding
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # name 过滤类别，attrs 过滤属性
+    links = soup.find(name='div', attrs={"class": "rebox_news"})        # find_all 的区别在于 limit=1
+    aas = links.find_all('a', href=True)        # 简洁语法
+    contents = []
+    for a in aas:
+        try:
+            href = a['href']        # 访问属性
+            contents.append(get_oneday(href[1:]))
+        except Exception as e:
+            print(e)
+    return contents
+
+def get_oneday(href):
+    response = requests.get(prefix+href)
+    response.encoding = "utf8"
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = soup.find(class_="vibox")
+    return {
+        "date": content.find(name='span', attrs={"id": "News_Body_Time"}).text,
+        "text": content.text        # text 获取网页渲染内容
+    }
+
+contents = []
+for i in tqdm(range(25, 7, -1)):
+# for i in [25]:
+    contents.extend(get_onelist(listpage.format(i)))
+
+with open("../data/Waijiaobu/wjb_2020.txt", "w", encoding="utf-8") as f:
+    for c in contents:
+        f.write(json.dumps(c, ensure_ascii=False) + "\n")
